5262b8093194d5e61ffffdf39fb98079439bfa12
asmabatttikhi <>
2021-06-02 14:18:13 +0000

    finishjavas

---
+++ app.js
@@ -0,0 +1,15 @@
+alert ('welcom to furniture design')
+var email = prompt ("sign up your email for latest trends")
+alert ('your email has been successfully signed ' + email)
+document.write('<h3'+email+'<h3>')
+var room = prompt ('choose the room you want to design')
+
+if (room === room)
+  {
+  alert ('room is found')
+  }
+  else
+  {
+  ("room not found")
+  }
+  
